bc5ea91864f8f85043e5614dcdb891029749faef
E5-E5 <69043407+E5-E5@users.noreply.github.com>
2021-04-25 22:23:15 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from data.players import Player
 import os
 import requests
 
-TOKEN = "ODMxOTA0MjE2NDg1MTk5ODky.YHcBLQ.AKORvfWyCYteQbz0S15wdmNi4uw"
+TOKEN = "Token"
 
 
 class Tic_Tac_Toe(commands.Cog):
